4c43f7f1a1754850d739189396d432ab3f173c3e
RaelMartinss <raelpgm2@gmail.com>
2024-04-02 14:05:05 -0300

    ajuste conf

--- node/index.js
+++ node/index.js
@@ -1,23 +1,58 @@
 const express = require('express')
+const { names, uniqueNamesGenerator } = require('unique-names-generator');
+const mysql = require('mysql2')
+
 const app = express()
 const port = 3000
+
 const config = {
     host: 'db',
     user: 'root',
     password: 'root',
     database:'nodedb'
 };
-const mysql = require('mysql')
+
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO peoples(name) values('Martins de sousa')`
-const sqlname = connection.query(`SELECT * FROM peoples`)
-connection.query(sql)
-connection.end()
+// const sql = `INSERT INTO people(name) values('Martins de sousa')`
+// // const sqlname = connection.query(`SELECT * FROM people`)
+// connection.query(sql)
+// connection.end()
 
+const createPerson = (name) => {
+    return new Promise((resolve, reject) => {
+        connection.query('INSERT INTO people SET ?', { name }, (err) => {
+            if (err) {
+                console.error(err)
+                return reject(err)
+            }
+            resolve()
+        })
+    })
+}
+
+const getPeople = () => {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM people',(err, results) => {
+            if(err) {
+                console.error(err)
+                return reject(err)
+            }
+            resolve(results)
+        })
+    })
+    
+}
+
+const makePeopleList = (people) => {
+    return `<ul>${people.map((item) => `<li>${item.name}</li>`).join('')}</ul>`
+}
 
 app.get('/', async (req,res) => {
-    res.send('<h1>Full Cycle DevOps@!!!!</h1>' + '<p>' + sqlname + '</p>')
+    const randoName = uniqueNamesGenerator({ dictionaries: [names] });
+    await createPerson(randoName)
+    const people = await getPeople()
+    res.send('<h1>God of War!!!!</h1>' + makePeopleList(people))
 })
 
 
